20f01219899fb93914ebe9024d12f26e95baa83f
Peter Vertenten <peter.vertenten@gmail.com>
2015-08-06 18:48:52 -0400

    making fat binary

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 xcodebuild -configuration Release -target 'Disk Image' PLCRASHREPORTER_PREFIX=Testfire
-
+lipo -output libCrashReporter.a -create build/Release-iphoneos/libCrashReporter-iphoneos.a  build/Release-iphonesimulator/libCrashReporter-iphonesimulator.a
